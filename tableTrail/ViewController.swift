6db3ee49288736e2b45020f06667cd1d510f6973
Gouthami-R <131648014+Gouthami-R@users.noreply.github.com>
2023-06-13 14:41:47 +0530

    converted to MVVM (#1)

--- tableTrail/ViewController.swift
+++ tableTrail/ViewController.swift
@@ -10,39 +10,32 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var pokemonTable: UITableView!
-    let dataManager = PokemonModel()
-    var pokeImages = NSCache<NSString,UIImage>()
-    var resultArray:[DataResult]? = [DataResult](){
-        didSet {
-            DispatchQueue.main.async {
-                self.pokemonTable.reloadData()
-            }
-        }
-    }
-    var image: UIImage? = UIImage() {
-        didSet {
-            DispatchQueue.main.async {
-                self.pokemonTable.reloadData()
-            }
-        }
-    }
+    
+    var viewmodel: PokeViewModelProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewmodel = PokeViewModel()
+//        viewmodel = goutmVM()
+        
+        viewmodel.fetchPokemon()
+        viewmodel.reloadTableView! = {
+            [weak self] in
+            DispatchQueue.main.async {
+                self?.pokemonTable.reloadData()
+            }
+        }
         title = "POKEMONs"
         pokemonTable.dataSource = self
         pokemonTable.delegate = self
-        dataManager.fetchPokeList { response in
-            if let result = response{
-                self.resultArray = result.results
-            }
-        }
+        
     }
     
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let count = resultArray?.count else {
+        guard let count = viewmodel.resultArray?.count else {
             return 1
         }
         return count
@@ -51,22 +44,15 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
         var content  = cell.defaultContentConfiguration()
-        if let result = resultArray, let pokename = result[indexPath.row].name {
-            content.text = pokename.capitalized
+        if let result = viewmodel.resultArray {
+            content.text = result[indexPath.row].name?.capitalized
             content.secondaryText = result[indexPath.row].url
             content.imageProperties.maximumSize = CGSize(width: 50, height: 50)
-            if let cached = pokeImages.object(forKey: NSString(string: pokename)) {
-                content.image = cached
-                cell.contentConfiguration = content
-            } else {
-                dataManager.fetchPokeImage(pokeNo: indexPath.row) {res in
-                    content.image = res
-                    DispatchQueue.main.async {
-                        self.pokeImages.setObject(res, forKey: NSString(string: pokename))
-                        cell.contentConfiguration = content
-                    }
+            content.image = viewmodel.fetchPokeImages(pokeNo: indexPath.row)
+                DispatchQueue.main.async {
+                    cell.contentConfiguration = content
                 }
-            }
+//            }
         }
         return cell
     }
@@ -77,7 +63,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         if let content = cell?.contentView.subviews[0] as? UIImageView {
             detailVC.image = content.image
         }
-        detailVC.title = resultArray?[indexPath.row].name?.capitalized
+//        detailVC.title = resultArray?[indexPath.row].name?.capitalized
         
         navigationController?.pushViewController(detailVC, animated: true)
     }
